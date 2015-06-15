cb61e4e55b245340089c505d3794d56599e51a64
John Kamenik <jkamenik@gmail.com>
2015-06-15 11:17:27 -0400

    Adding my custom variables

---
+++ custom.el
@@ -0,0 +1,16 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files
+   (quote
+    ("~/Dropbox/org-mode/thotcon.org" "~/Dropbox/org-mode/using-orgmode.org")))
+ '(org-src-fontify-natively t)
+ '(tabbar-separator (quote (0.5))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
