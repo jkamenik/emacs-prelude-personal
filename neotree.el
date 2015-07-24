522050fdb7fb51fc946d7c3fa816443e10e85add
John Kamenik <jkamenik@gmail.com>
2015-07-24 10:36:31 -0400

    Disable auto open of neotree

--- neotree.el
+++ neotree.el
@@ -3,6 +3,6 @@
 
 (setq projectile-switch-project-action 'neotree-projectile-action)
 (global-set-key [f8] 'neotree-toggle)
-(neotree-show) ;; show by default
+;; (neotree-show) ;; show by default
 
 (provide 'personal-neotree)
