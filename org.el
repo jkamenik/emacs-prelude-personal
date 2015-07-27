7637ea4881960c7a966ebb118e964874dc6e2965
John Kamenik <jkamenik@gmail.com>
2015-07-27 09:44:36 -0400

    Setup org-capture.

--- org.el
+++ org.el
@@ -4,4 +4,12 @@
           (lambda ()
             (prelude-mode -1)))
 
+(setq org-default-notes-file org-gtd-file)
+
+;; Setup capture templates for quick setup
+;; http://orgmode.org/manual/Capture-templates.html#Capture-templates
+(setq org-capture-templates
+      '(("t" "Todo" entry (file+headline org-gtd-file "To be filed")
+         "* TODO %?\nCaptured: %T\n%i")))
+
 (provide 'personal-org)
