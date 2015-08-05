50f885e4e9db0218443a6cf14e45de62e04178bf
John Kamenik <jkamenik@gmail.com>
2015-08-05 12:42:47 -0400

    Adding another capture type

--- org.el
+++ org.el
@@ -10,6 +10,21 @@
 ;; http://orgmode.org/manual/Capture-templates.html#Capture-templates
 (setq org-capture-templates
       '(("t" "Todo" entry (file+headline org-gtd-file "To be filed")
-         "* TODO %?\nCaptured: %T\n%i")))
+         "* TODO %?  :WORK:\n- Captured: %T\n%i")
+        ("c" "Competition" entry (file+headline org-gtd-file "Competition")
+         "* %t
+** Points
+- [ ] Breakfast, lunch, dinner, and night snack
+- [ ] 5000 steps
+- [ ] Daily exercise
+- [ ] 8 hours of sleep
+- [ ] Tracking food
+- [ ] 8 Cups of water
+
+** Negative
+- [ ] Unplanned Eating out
+- [ ] Other Snacks
+- [ ] Procrastination
+")))
 
 (provide 'personal-org)
